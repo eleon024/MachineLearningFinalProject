06183ea458dace900da62447c8196df8f2777f7f
Will Betts Cope <wbettscope@haverford.edu>
2025-05-08 16:36:59 -0400

    added testing environment and edited figure creation in simple_snake

--- snake-ai-pytorch/simple_snake/helper_simple.py
+++ snake-ai-pytorch/simple_snake/helper_simple.py
@@ -3,11 +3,11 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, mean_scores,save=False,filename="figure"):
+def plot(scores, mean_scores, title, filename, filepath, save=True):
     display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
-    plt.title('Traditional Snake Traning: Score vs Number of games')
+    plt.title(title)
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
     plt.plot(scores, label="scores")
@@ -20,4 +20,4 @@ def plot(scores, mean_scores,save=False,filename="figure"):
     plt.pause(.1)
 
     if (save):
-        plt.savefig("figures/"+str(filename)+".svg", format="svg")
+        plt.savefig(filepath+str(filename)+".svg", format="svg")
