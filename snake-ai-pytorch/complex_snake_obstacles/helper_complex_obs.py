81aeb794c42c4c43c2fb4bf1b02e252a735e33a4
Rachel November <rachelnovember3@gmailcom>
2025-05-09 10:21:16 -0400

    Rachel edits 10:20

--- snake-ai-pytorch/complex_snake_obstacles/helper_complex_obs.py
+++ snake-ai-pytorch/complex_snake_obstacles/helper_complex_obs.py
@@ -23,10 +23,9 @@ def plot(scores, mean_scores,save=False,filename="figure"):
         plt.savefig("figures/"+str(filename)+".svg", format="svg")
 
 
-
+fig, axs = plt.subplots(1, 3, figsize=(12, 5))
+display.display(fig)
 def plot_combined(scores, mean_scores, times, speeds):
-    fig, axs = plt.subplots(1, 3, figsize=(12, 5))
-    display.display(fig)
     axs[0].cla()
     axs[0].set_title('Score')
     axs[0].plot(scores, label='Score')
